b0e0db47ccf7a3c9b0b1df33a73d73c94059712d
ZY <1138897995@qq.com>
2022-07-13 18:28:26 +0800

    添加备注

--- day01/src/HelloWorld.java
+++ day01/src/HelloWorld.java
@@ -1,3 +1,10 @@
+/**
+ * @author zhangyu
+ * @describe (learning java)
+ * @day01
+ * @date 2022/7/13 17:50
+ */
+
 public class HelloWorld {
     public static void main(String[] args) {
         System.out.println("day01");
